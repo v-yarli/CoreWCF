b34f7f0ee78c850134abb201fd40cb89f1781b3e
Yaru Li (Inspur Worldwide Services Ltd) <v-yarli@microsoft.com>
2020-06-17 22:53:54 -0700

    OneWaypatternTest

---
+++ src/CoreWCF.Http/tests/OneWayPatternTest.cs
@@ -0,0 +1,57 @@
+﻿using CoreWCF.Configuration;
+using Helpers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace BasicHttp
+{
+    public class OneWayPatternTest
+    {
+        private ITestOutputHelper _output;
+        public OneWayPatternTest(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+#if NET472
+        [Fact]
+        public void InvokeTaskBasedOneway()
+        {
+            var host = ServiceHelper.CreateWebHostBuilder<Startup>(_output).Build();
+            using (host)
+            {
+                host.Start();
+                var httpBinding = ClientHelper.GetBufferedModeBinding();
+                var factory = new System.ServiceModel.ChannelFactory<ClientContract.IOneWayContract>(httpBinding,
+                    new System.ServiceModel.EndpointAddress(new Uri("http://localhost:8080/OneWayPattern/basichttp.svc")));
+                var channel = factory.CreateChannel(); 
+                Task task = channel.OneWay("Hello");
+                task.Wait();
+            }
+        }
+#endif
+        internal class Startup
+        {
+            public void ConfigureServices(IServiceCollection services)
+            {
+                services.AddServiceModelServices();
+            }
+
+#pragma warning disable IDE0060 // Remove unused parameter
+            public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+#pragma warning restore IDE0060 // Remove unused parameter
+            {
+                app.UseServiceModel(builder =>
+                {
+                    builder.AddService<Services.OneWayService>();
+                    builder.AddServiceEndpoint<Services.OneWayService, ServiceContract.IOneWayContract>(new CoreWCF.BasicHttpBinding(), "/OneWayPattern/basichttp.svc");
+                });
+            }
+        }
+    }
+}
---
+++ src/CoreWCF.Http/tests/Services/OneWayService.cs
@@ -0,0 +1,25 @@
+﻿using ServiceContract;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+using CoreWCF;
+using Xunit.Sdk;
+
+namespace Services
+{
+	[ServiceBehavior]
+	public class OneWayService : IOneWayContract
+	{
+		// Token: 0x06000865 RID: 2149 RVA: 0x0002026C File Offset: 0x0001E46C
+		private ITestOutputHelper _output= new TestOutputHelper();
+
+		public Task OneWay(string s)
+		{
+			Task task = new Task(delegate
+            {
+				_output.WriteLine(string.Format("Invoked oneway operation with {0}.", s));
+			});
+			task.Start();
+			return task;
+		}
+	}
+}
